908b1512676b1b9c110e3edc336b4c0c86e43dab
ArnoAlford <35819974+ArnoAlford@users.noreply.github.com>
2018-10-06 02:16:23 -0700

    Hidden toggle between divs

--- js/arno.js
+++ js/arno.js
@@ -1,8 +1,11 @@
 function myFunction() {
     var x = document.getElementById("myDIV");
+    var y = document.getElementById("myDIV2");
     if (x.style.display === "none") {
+        y.style.display = "none";
         x.style.display = "block";
     } else {
         x.style.display = "none";
+        y.style.display = "block";
     }
 }
