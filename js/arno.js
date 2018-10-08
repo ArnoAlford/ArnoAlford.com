235540e1882bb81bd46062dcf33105313de7fb9d
ArnoAlford <35819974+ArnoAlford@users.noreply.github.com>
2018-10-07 17:55:19 -0700

    Lots of changes to First SQL Project Page

--- js/arno.js
+++ js/arno.js
@@ -9,3 +9,18 @@ function myFunction() {
         y.style.display = "block";
     }
 }
+
+function myFunction2() {
+    var x = document.getElementById("myDIV2");
+    var y = document.getElementById("myDIV3");
+    var z = document.getElementById("myDIV5");
+    if (x.style.display === "none") {
+        y.style.display = "none";
+        z.style.display = "none";
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+        z.style.display = "block";
+    }
+}
