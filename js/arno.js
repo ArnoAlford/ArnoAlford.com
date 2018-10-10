98c81e3c0bd7df59f55b3d1be6879ccfe78a8d57
ArnoAlford <35819974+ArnoAlford@users.noreply.github.com>
2018-10-10 01:25:26 -0700

    Finished FLARE page and added problem page

--- js/arno.js
+++ js/arno.js
@@ -27,6 +27,30 @@ function myFunction2() {
 
 function myFunction3() {
     var x = document.getElementById("myDIV2");
+    var y = document.getElementById("myDIV7");
+    if (x.style.display === "none") {
+        y.style.display = "none";
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
+}
+
+function myFunction4() {
+    var x = document.getElementById("myDIV7");
+    var y = document.getElementById("myDIV6");
+    if (x.style.display === "none") {
+        y.style.display = "none";
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
+}
+
+function myFunction5() {
+    var x = document.getElementById("myDIV2");
     var y = document.getElementById("myDIV6");
     if (x.style.display === "none") {
         y.style.display = "none";
